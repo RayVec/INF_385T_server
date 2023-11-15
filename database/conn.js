40214cea47c9334aaee7a2c8fb96c3b98d0b816c
RayVec <34103363+RayVec@users.noreply.github.com>
2023-11-15 10:08:37 -0600

    1

--- database/conn.js
+++ database/conn.js
@@ -1,31 +1,31 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://oco0017:inf385TFall@cluster0.mea69ac.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://rayvec:rayvec@cluster0.e6oynif.mongodb.net/?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 var dataBase;
 
 module.exports = {
-
-connectToServer: async function () {
+  connectToServer: async function () {
     client.connect();
     // Send a ping to confirm a successful connection
-    
-    client.db("admin").command({ ping: 1 });
-    
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    
-    dataBase = client.db('eCommerce');   
-    },
 
-getDb: function (){
+    client.db("admin").command({ ping: 1 });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    dataBase = client.db("eCommerce");
+  },
+
+  getDb: function () {
     return dataBase;
   },
 };
-
-  
